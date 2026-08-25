5b80abf2aa5763a13f9a21017f79bc104868a0e2
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-08-25 14:45:35 +0200

    Remove printing of chunk removal

--- client/World-Scene/world.cpp
+++ client/World-Scene/world.cpp
@@ -140,7 +140,6 @@ void World::update_chunks() {
         }
     }
     for (const ChunkPos& pos : chunks_to_remove) {
-        std::cout << "Removing chunk " << pos.x << ", " << pos.z << "\n";
         const Chunk& chunk = chunks.at(pos);
         scene.remove_object_from_scene(chunk.object.get());
         renderer.destroy_mesh(*chunk.mesh);
